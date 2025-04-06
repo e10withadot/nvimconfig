471447f868439f31d021aec0372a5443d0e24ef0
e10withadot <e10withadot@gmail.com>
2025-04-06 12:59:18 +0300

    virtual text only appears on error

--- lua/plugins/nvim-lspconfig/diagnostics.lua
+++ lua/plugins/nvim-lspconfig/diagnostics.lua
@@ -16,15 +16,7 @@ return function()
     virtual_text = {
       source = 'if_many',
       spacing = 2,
-      format = function(diagnostic)
-        local diagnostic_message = {
-          [vim.diagnostic.severity.ERROR] = diagnostic.message,
-          [vim.diagnostic.severity.WARN] = diagnostic.message,
-          [vim.diagnostic.severity.INFO] = diagnostic.message,
-          [vim.diagnostic.severity.HINT] = diagnostic.message,
-        }
-        return diagnostic_message[diagnostic.severity]
-      end,
+      severity = vim.diagnostic.severity.ERROR,
     },
   }
   vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
