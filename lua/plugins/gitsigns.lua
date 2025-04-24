a539f5e8699e3089b3a4d4430644bfddc4352960
e10withadot <e10withadot@gmail.com>
2025-04-24 13:16:15 +0300

    remove unnecessary gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,15 +2,6 @@ return {
   -- See `:help gitsigns` to understand what the configuration keys do
   {
     'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
     config = function()
       -- set colors before calling setup
       vim.cmd [[ highlight GitSignsAdd guifg=green ]]
