9be6113478967e428b76871dddb362b3fbdf2e3f
e10withadot <e10withadot@gmail.com>
2025-09-14 10:08:07 +0300

    added lighter tabline plugin

---
+++ lua/plugins/tabline.lua
@@ -0,0 +1,16 @@
+return {
+  'crispgm/nvim-tabline',
+  dependencies = { 'nvim-tree/nvim-web-devicons' }, -- optional
+  config = function()
+    require('tabline').setup {
+      show_index = true,
+      show_modify = true,
+      show_icon = true,
+      fnamemodify = ':t',
+      modify_indicator = '+',
+      no_name = 'Undefined',
+      brackets = { ' ', ' ' },
+      inactive_tab_max_length = 0,
+    }
+  end,
+}
