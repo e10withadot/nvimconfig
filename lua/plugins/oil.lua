edc18fa8a82a53f31924036fc3f75d835a76df02
e10withadot <e10withadot@gmail.com>
2025-10-13 12:57:22 +0300

    replace neo-tree with oil

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,19 @@
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  config = function()
+    require('oil').setup {
+      default_file_explorer = true,
+      view_options = {
+        show_hidden = true,
+      },
+    }
+    vim.keymap.set('n', '-', require('oil').toggle_float, { desc = 'Open Oil' })
+  end,
+  -- Optional dependencies
+  dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+}
