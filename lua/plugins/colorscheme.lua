8ac9984ee0075047d8acfa029b974119e192de93
e10withadot <e10withadot@gmail.com>
2025-12-21 01:25:20 +0200

    minor syntax improvements

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,14 +4,13 @@ return {
   'EdenEast/nightfox.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
-    local specs = {
+    require('nightfox').setup { specs = {
       carbonfox = {
         diag = {
           warn = '#ffce20',
         },
       },
-    }
-    require('nightfox').setup { specs = specs }
+    } }
 
     -- Load the colorscheme here.
     vim.cmd.colorscheme 'carbonfox'
