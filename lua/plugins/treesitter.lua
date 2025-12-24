92d3bfd5009e482ec8c3c0522d55b6bb1a288827
e10withadot <e10withadot@gmail.com>
2025-12-23 19:38:51 -0800

    specify nvim-treesitter main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -34,6 +34,7 @@ end
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
+  branch = 'main',
   build = ':TSUpdate',
   config = function()
     require('nvim-treesitter').install(ensure_installed)
