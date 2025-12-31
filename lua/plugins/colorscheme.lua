ba8410ab2ec51aa3d50166c766c033de5ae25978
e10withadot <e10withadot@gmail.com>
2025-12-31 09:55:48 +0200

    make colors transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,7 +4,11 @@ return {
   'EdenEast/nightfox.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
-    require('nightfox').setup { specs = {
+    require('nightfox').setup { 
+      options = {
+        transparent = true
+      },
+      specs = {
       carbonfox = {
         diag = {
           warn = '#ffce20',
