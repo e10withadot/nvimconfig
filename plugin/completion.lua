96ffe29a1255530525712731f0c0ede00b942ccd
e10withadot <e10withadot@gmail.com>
2026-03-30 20:04:43 +0300

    remove blink's icons

--- plugin/completion.lua
+++ plugin/completion.lua
@@ -12,7 +12,34 @@ local blink = require('blink.cmp')
 blink.setup {
     keymap = { preset = 'default' },
     appearance = {
-        nerd_font_variant = 'mono'
+    -- remove all icons
+      kind_icons = {
+        Text = '',
+        Method = '',
+        Function = '',
+        Constructor = '',
+        Field = '',
+        Variable = '',
+        Property = '',
+        Class = '',
+        Interface = '',
+        Struct = '',
+        Module = '',
+        Unit = '',
+        Value = '',
+        Enum = '',
+        EnumMember = '',
+        Keyword = '',
+        Constant = '',
+        Snippet = '',
+        Color = '',
+        File = '',
+        Reference = '',
+        Folder = '',
+        Event = '',
+        Operator = '',
+        TypeParameter = '',
+      },
     },
     completion = { documentation = { auto_show = true } },
     sources = {
