d5c6823096708e4e95ca8ff19224fbd4d43e1a76
e10withadot <e10withadot@gmail.com>
2026-03-30 20:00:57 +0300

    added native completion settings

--- plugin/completion.lua
+++ plugin/completion.lua
@@ -1,10 +1,11 @@
 vim.pack.add {
     {
         src = 'https://www.github.com/saghen/blink.cmp', -- all-in-one completion engine
-        version = vim.version.range('1.0.0 - 2.0.0'),
+        version = vim.version.range('1.x'),
     }
 }
--- Don't show autocompletion warnings
+-- native completion settings
+vim.opt.completeopt = 'menu,fuzzy,noinsert,popup'
 vim.opt.shortmess:append 'c'
 
 local blink = require('blink.cmp')
@@ -13,10 +14,9 @@ blink.setup {
     appearance = {
         nerd_font_variant = 'mono'
     },
-    completion = { documentation = { auto_show = false } },
+    completion = { documentation = { auto_show = true } },
     sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
     },
     fuzzy = { implementation = "prefer_rust_with_warning" }
 }
-vim.keymap.set('i', '<C-x><C-o>', blink.show_and_insert)
