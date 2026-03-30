9f8aae22a90d230350809f3b478116726a926bb0
e10withadot <e10withadot@gmail.com>
2026-03-30 22:37:30 +0300

    fixed treesitter autoinstall

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -19,38 +19,34 @@ local ts = require('nvim-treesitter')
 local ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
 ts.install(ensure_installed)
 
-local treesitter_start = function(buf)
-  pcall(vim.treesitter.start)
+local treesitter_start = function(buf, lang)
+  pcall(vim.treesitter.start, buf, lang)
   vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 end
 
 local treesitter_install = function(buf, lang)
-  -- check if parser is available
-  local get_available = ts.get_available()
-  for _, l in ipairs(get_available) do
-    if l == lang then
-      -- install parser
-      ts.install(lang):await(function()
-        treesitter_start(buf)
-      end)
-      -- check again
-      if not vim.treesitter.start() then
-        return
-      end
+  local installed = ts.get_installed()
+  if not vim.list_contains(installed, lang) then
+    local ok, err = pcall(ts.install, lang)
+    if not ok then
+      vim.notify('Failed to install parser: ' .. lang .. ' - ' .. tostring(err), vim.log.levels.ERROR)
+      return
     end
   end
+  treesitter_start(buf, lang)
 end
 
-
+-- automatically install and start treesitter
 vim.api.nvim_create_autocmd('FileType', {
   group = vim.api.nvim_create_augroup('treesitter.setup', {}),
   callback = function(event)
-    local filetype = event.match
-    local lang = vim.treesitter.language.get_lang(filetype) or filetype
+    local lang = vim.treesitter.language.get_lang(event.match) or event.match
+    local available = ts.get_available()
+    if not vim.list_contains(available, lang) then return end
     if not vim.treesitter.language.add(lang) then
       treesitter_install(event.buf, lang)
     end
-    treesitter_start(event.buf)
+    treesitter_start(event.buf, lang)
   end,
 })
 
@@ -59,7 +55,7 @@ vim.api.nvim_create_user_command('TSStart', function()
   local filetype = vim.bo.filetype
   local lang = vim.treesitter.language.get_lang(filetype) or filetype
   if lang then
-    treesitter_start(buf)
+    treesitter_start(buf, lang)
   else
     vim.notify('Language not available.', vim.log.levels.WARN, {})
   end
